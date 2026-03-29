a9002f534e6610dd684d0ed6e4cfa6d1fa80033e
Filip Flejsierowicz <s26047@pjwstk.edu.pl>
2026-03-29 23:03:47 +0200

    Dodano metodę CalculateAverage

--- Program.java
+++ Program.java
@@ -2,4 +2,14 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Witaj świecie");
     }
+
+    public void CalculateAverage(int[] values) {
+        int sum = 0;
+        double average = 0;
+        for(int value : values){
+            sum += value;
+        }
+        average = (double) sum / values.length;
+        System.out.println(average);
+    }
 }
